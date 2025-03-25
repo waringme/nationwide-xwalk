cbbf3a425fbf1d887fa238343f7b757f3e5d6461
Tim Waring <waring@adobe.com>
2025-03-25 15:56:54 +0000

    Update content-fragment.js

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -8,7 +8,8 @@ function getMetadata(name) {
   
   //const aem = "http://localhost:4503";
   //const aem = "https://publish-p107058-e1001010.adobeaemcloud.com";
-  const aem = "https://publish-p135360-e1341441.adobeaemcloud.com/";
+  //const aem = "https://publish-p135360-e1341441.adobeaemcloud.com/";
+const aem = "https://publish-p150634-e1553296.adobeaemcloud.com";
   
   export default function decorate(block) {
   
@@ -21,20 +22,20 @@ function getMetadata(name) {
     destinationDiv.id = `destination-${slugID.textContent}`;
     block.querySelector('div:last-of-type').replaceWith(destinationDiv);
   
-    fetch(`${aem}/graphql/execute.json/qatar-airways/destination-by-slug;slug=${slugID.textContent}`)
+    fetch(`${aem}/graphql/execute.json/nationwide/offer-by-slug;slug=${slugID.textContent}`)
       .then(response => response.json())
       .then(response => {
-        const { primaryImage, cityName, cityNickName, cityDescription } = response.data.travelDestinationList.items[0];
+        const { bannerImage } = response.data.travelDestinationList.items[0];
         const imageURL = `${aem}${primaryImage._dynamicUrl}`;
   
         destinationDiv.innerHTML = `
           <div class='destination-image'>
-            <img src="${imageURL}" alt="${cityName}">
+            <img src="${imageURL}" alt="offer"}">
           </div>
           <div class='destination-content'>
-            <div class='destination-content-title'><h3>${cityName}</h3></div>
-            <div class='destination-content-subtitle'><h4>${cityNickName}</h4></div>
-            <div class='destination-content-description'><p>${cityDescription.plaintext}</p></div>
+            <div class='destination-content-title'><h3>Test</h3></div>
+            <div class='destination-content-subtitle'><h4>Test</h4></div>
+            <div class='destination-content-description'><p>test</p></div>
           </div>
         `;
       })
